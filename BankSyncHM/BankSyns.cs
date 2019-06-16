fcb1f0e8cbd6a6ef72d30f4cc5341ce8a5c56dee
DexiGG <invokedex@gmail.com>
2019-06-17 01:58:14 +0600

    banksysHM

---
+++ BankSyncHM/BankSyns.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BankSyncHM
+{
+    public class BankSyns
+    {
+        public int Money { get; set; }
+        private object lockObject = new object();
+        private int Transactions;
+
+        public void MoneyMOVE(object state)
+        {
+            lock (lockObject)
+            {
+            var currentThread = Thread.CurrentThread;
+            Console.WriteLine($"\n{currentThread.ManagedThreadId} начал свою работу c {Money}");
+
+            int number = new Random().Next(0, 1000);
+
+            Thread.Sleep(500);
+
+            if (number > 50)
+            {
+                Money -= 100;
+                Transactions -= 100;
+
+            }
+            else
+            {
+                Money += 100;
+                Transactions += 100;
+
+            }
+            Console.WriteLine($"\n+-Тенге ({Transactions}) Операция ");
+            Console.WriteLine($"\n{currentThread.ManagedThreadId} закончил свою работу {Money}");
+           }
+        }
+    }
+}
---
+++ BankSyncHM/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BankSyncHM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var threads = new Thread[5];
+            var bank = new BankSyns();
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                var thread = new Thread(bank.MoneyMOVE);
+                threads[i] = thread;
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
